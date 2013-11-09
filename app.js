60795a1323596000d2f1a0ab48e59424aa5cd787
Max Bucknell <max.bucknell@redboxdigital.com>
2013-11-09 23:24:43 +0000

    Remove more crap and build scaffold.

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
-var app = express();
+  , path = require('path')
+  , app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('port', 29722);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/posts', routes.getPosts);
+app.post('/posts', routes.postPosts);
+app.put('/posts', routes.putPosts);
+
+app.get('/posts/:id', routes.getPost);
+app.put('/posts/:id', routes.putPost);
+app.delete('/posts/:id', routes.deletePost);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes.js
@@ -0,0 +1,25 @@
+var util = require('util');
+
+exports.getPosts = function (req, res) {
+
+};
+
+exports.postPosts = function (req, res) {
+
+};
+
+exports.putPosts = function (req, res) {
+
+};
+
+exports.getPost = function (req, res) {
+  
+};
+
+exports.putPost = function (req, res) {
+
+};
+
+exports.deletePost = function (req, res) {
+
+};
