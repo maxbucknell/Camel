f6048ccd9d92b71a95b88c691607789de45cde28
Max Bucknell <max.bucknell@redboxdigital.com>
2013-11-10 23:03:00 +0000

    Condense into one file and add a lovely header.

--- app.js
+++ app.js
@@ -1,29 +1,83 @@
-var express = require('express')
-  , routes = require('./routes.js')
-  , http = require('http')
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//     //
+//  # Camel.                                    //    _oo\
+// ## A new take on blogging.                  //    (__/ \  _  _
+//    Because it's about time somebody did    //        \  \/ \/ \
+//    something that isn't just a reverse    //         (         )\
+//    chronological list of articles        //           \_______/  \
+//                                         //             [[] [[]
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//              [[] [[]
+
+~function () {
+  var express = require('express')
+    , http    = require('http')
+    
+    , SomethingWentWrongError = function (statusCode, message, req) {
+        try {
+          this.statusCode = req.query.suppress_response_codes ? 200 : statusCode
+        } catch (e) {
+          this.statusCode = statusCode
+        }
+        
+        this.message = {
+            status  : statusCode
+          , message : message || 'Some ignorant slut didn\'t think you important enough to get a decent error message.'
+        }
+      }
+    
+    , unusedEndpointError = new SomethingWentWrong(405, 'The endpoint you asked for exists, but does not respond to that method.')
+    , notAuthenticatedError = new SomethingWentWrong(401, 'Please provide a valid API signature.')
+    , notAuthorisedError = new SomethingWentWrong(403, 'That API signature does not grant you to access that resource.')
+    
+    , dispatch = function (dir, auth) {
+        dir = dir[0].toUpper() + dir.slice(1)
+        return function (req, res) {
+          var signature = req.query.signature
+            , authenticated = isSigValid(signature)
+            , authorised = isSigValidForResource(signature, req.route, auth)
+            
+            , outputFunc = routes[req.route.method + dir] || unusedEndpoint
+          
+          va
+          
+          try {
+            res.send(valid ? 
+          } catch (e) {
+            // We know about these
+            if (e.statusCode) res.send(e.statusCode, e.message)
+            // Something else happened
+            else              throw e;
+          }
+        }
+      }
+    
+    , routes = {
+        getPosts    : function (req) {
+          return ['hello', 'world']
+        }
+      , postPosts   : function (req) {
+          return ['hello', 'world']
+        }
+      , putPosts    : function (req) {
+          return ['hello', 'world']
+        }
+        
+      , getPost     : function (req) {
+          return ['hello', 'world']
+        }
+      , putPost     : function (req) {
+          return ['hello', 'world']
+        }
+      , deletePost  : function (req) {
+          return ['hello', 'world']
+        }
+    }
+    
+    , app = express()
   
-  , app = express()
-
-app.configure(function () {
-  app.set('port', 29722)
-  app.use(express.logger('dev'))
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(app.router)
-})
-
-app.configure('development', function () {
-  app.use(express.errorHandler())
-})
-
-app.get('/posts', routes.getPosts)
-app.post('/posts', routes.postPosts)
-app.put('/posts', routes.putPosts)
-
-app.get('/posts/:id', routes.getPost)
-app.put('/posts/:id', routes.putPost)
-app.delete('/posts/:id', routes.deletePost)
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'))
-})
+  app.enable('trust proxy')
+  
+  app.all('/posts', dispatch('posts', ['put', 'post', 'delete']))
+  app.all('/posts/:id', dispatch('posts', ['put', 'post', 'delete']))
+  
+  http.createServer(app).listen(29722)
+}()
