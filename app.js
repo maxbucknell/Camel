ca7f972804ed28d5299577323e318f4513eeb735
Max Bucknell <max.bucknell@redboxdigital.com>
2013-11-10 15:44:47 +0000

    Delete more crap.
    In a stirring turn of events, I've gotten rid of more shit that
    I don't need.
    
    I also rewrote the routes file. I'm trying this thing where I
    write the most hipster code that I can think of. Inspired by fat.

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path')
-  , app = express();
+  
+  , app = express()
 
-app.configure(function(){
-  app.set('port', 29722);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
+app.configure(function () {
+  app.set('port', 29722)
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+})
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.configure('development', function () {
+  app.use(express.errorHandler())
+})
 
-app.get('/posts', routes.getPosts);
-app.post('/posts', routes.postPosts);
-app.put('/posts', routes.putPosts);
+app.get('/posts', routes.getPosts)
+app.post('/posts', routes.postPosts)
+app.put('/posts', routes.putPosts)
 
-app.get('/posts/:id', routes.getPost);
-app.put('/posts/:id', routes.putPost);
-app.delete('/posts/:id', routes.deletePost);
+app.get('/posts/:id', routes.getPost)
+app.put('/posts/:id', routes.putPost)
+app.delete('/posts/:id', routes.deletePost)
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+  console.log("Express server listening on port " + app.get('port'))
+})
